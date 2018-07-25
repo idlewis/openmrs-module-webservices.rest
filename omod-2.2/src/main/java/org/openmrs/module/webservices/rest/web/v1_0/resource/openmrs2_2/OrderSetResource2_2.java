2af6ad22d02090ad9e9cca4e91d9ec65cc4a3b67
ilewis <idlewis@users.noreply.github.com>
2018-07-25 01:48:57 +0100

    Update ordersetresource with attributes

--- omod-2.2/src/main/java/org/openmrs/module/webservices/rest/web/v1_0/resource/openmrs2_2/OrderSetResource2_2.java
+++ omod-2.2/src/main/java/org/openmrs/module/webservices/rest/web/v1_0/resource/openmrs2_2/OrderSetResource2_2.java
@@ -9,12 +9,81 @@
  */
 package org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs2_2;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.openmrs.OrderSet;
+import org.openmrs.OrderSetAttribute;
 import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.openmrs.module.webservices.rest.web.annotation.PropertySetter;
 import org.openmrs.module.webservices.rest.web.annotation.Resource;
+import org.openmrs.module.webservices.rest.web.representation.DefaultRepresentation;
+import org.openmrs.module.webservices.rest.web.representation.FullRepresentation;
+import org.openmrs.module.webservices.rest.web.representation.Representation;
+import org.openmrs.module.webservices.rest.web.resource.impl.DelegatingCrudResource;
+import org.openmrs.module.webservices.rest.web.resource.impl.DelegatingResourceDescription;
 import org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs1_12.OrderSetResource1_12;
 
-@Resource(name = RestConstants.VERSION_1 + "/orderset", supportedClass = OrderSet.class, supportedOpenmrsVersions = {"2.2.*" })
+@Resource(name = RestConstants.VERSION_1 + "/orderset", supportedClass = OrderSet.class, supportedOpenmrsVersions = { "2.2.*" })
 public class OrderSetResource2_2 extends OrderSetResource1_12 {
+	
+	
+	/**
+	 * Sets attributes on the given OrderSet.
+	 * 
+	 * @param instance
+	 * @param attrs
+	 */
+	@PropertySetter("attributes")
+	public static void setAttributes(OrderSet instance, List<OrderSetAttribute> attrs) {
+		for (OrderSetAttribute attr : attrs) {
+			instance.addAttribute(attr);
+		}
+	}
+	
+	/**
+	 * @see DelegatingCrudResource#getRepresentationDescription(Representation)
+	 */
+	@Override
+	public DelegatingResourceDescription getRepresentationDescription(Representation rep) {
+		if (rep instanceof DefaultRepresentation) {
+			DelegatingResourceDescription description = super.getRepresentationDescription(rep);
+			description.addProperty("attributes", Representation.REF);
+			return description;
+		} else if (rep instanceof FullRepresentation) {
+			DelegatingResourceDescription description = super.getRepresentationDescription(rep);
+			description.addProperty("attributes", Representation.DEFAULT);
+			return description;
+		}
+		return null;
+	}
+	
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.resource.impl.BaseDelegatingResource#getCreatableProperties()
+	 */
+	@Override
+	public DelegatingResourceDescription getCreatableProperties() {
+		DelegatingResourceDescription description = super.getCreatableProperties();
+		description.addProperty("attributes");
+		return description;
+	}
+	
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.resource.impl.BaseDelegatingResource#getPropertiesToExposeAsSubResources()
+	 */
+	@Override
+	public List<String> getPropertiesToExposeAsSubResources() {
+		return Arrays.asList("attributes");
+	}
+	
+	/**
+	 * @see org.openmrs.module.webservices.rest.web.resource.impl.BaseDelegatingResource#getResourceVersion()
+	 */
+	@Override
+	public String getResourceVersion() {
+		return "2.2";
+	}
+	
+
 	
 }
